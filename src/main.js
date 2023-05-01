4113fc181f00cfa95a41c69430dd62b36d394951
Sarman-Dasa <sarmand@zignuts.com>
2023-05-01 19:13:34 +0530

    import vuelidate package

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './routes'
 import "bootstrap/dist/css/bootstrap.css";
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
+import Vuelidate from "vuelidate";
 
-createApp(App).use(router,bootstrap).mount('#app')
+createApp(App).use(router,bootstrap,Vuelidate).mount('#app')
